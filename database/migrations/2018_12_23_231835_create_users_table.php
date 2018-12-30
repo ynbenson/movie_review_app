2dada853dd4149f406574f1e598b5a7a8466dde7
Yujiro <yujiro91224@gmail.com>
2018-12-30 16:58:14 -0500

    displays data from movie database

--- database/migrations/2018_12_23_231835_create_users_table.php
+++ database/migrations/2018_12_23_231835_create_users_table.php
@@ -14,7 +14,7 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('user_id');
             $table->string('name',32);
             $table->string('username',32);
             $table->string('email',320);
