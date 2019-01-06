5c7b6d4b4043311d64f547610ae37d7acfb3fa52
Yujiro <yujiro91224@gmail.com>
2019-01-06 00:50:02 -0500

    :sparkles: review feature complete

--- app/Http/Controllers/ReviewMoviesController.php
+++ app/Http/Controllers/ReviewMoviesController.php
@@ -9,33 +9,55 @@ use Illuminate\Support\Facades\Auth;
 
 class ReviewMoviesController extends Controller
 {
+    public function getMovie(){
+        $user_id = Auth::user()->id;
+        $reviewed_movies = DB::table('review_histories')
+                ->select('movie_id')
+                ->where('user_id',$user_id)
+                ->get();
+        $reviewed_movies_id = $reviewed_movies->pluck('movie_id')->all();
+
+        $movie = DB::table('movies')
+                ->whereNotIn('movie_id', $reviewed_movies_id)
+                ->orderByRaw("RAND()")
+                ->limit(1)
+                ->get();                       
+
+        if ($movie->count() === 0) {
+            return redirect()->route('home')->with('alert', 'You have reviewed all movies in database!');
+        }
+            
+        return view('reviewMovie', ['movie' => $movie]);   
+    }
+    
     public function index(){
         if (Auth::check()){
-            $user_id = Auth::user()->id;
-            $reviewed_movies = DB::table('review_histories')
-                    ->select('movie_id')
-                    ->where('user_id',$user_id)
-                    ->get();
-            $reviewed_movies_id = $reviewed_movies->pluck('movie_id')->all();
-            
-            $movies = DB::table('movies')
-                    ->whereNotIn('movie_id', $reviewed_movies_id)
-                    ->orderByRaw("RAND()")
-                    ->get();                       
-            
-            if ($movies->count() === 0) {
-                return redirect()->route('home')->with('alert', 'You have reviewed all movies in database!');
-            }
-            
-            //dd($movies);
-            //shuffle($movies);
-            return view('reviewMovie', ['movies' => $movies]);            
+            return $this->getMovie();
         } else {
             return redirect()->route('login');
         }
     }
     
     public function rateMovie(Request $request){
-        dd($request->all());
+        if ($request->btn === "exc"){
+            $review_score = 2;
+        } elseif ($request->btn === "great"){
+            $review_score = 1;
+        } elseif ($request->btn === "ok"){
+            $review_score = 0;
+        } elseif ($request->btn === "poor"){
+            $review_score = -1;
+        } else {
+            $review_score = -2;
+        }
+        
+        DB::table('review_histories')->insert([
+            'user_id'       =>Auth::user()->id,
+            'movie_id'      =>$request->movie_id,
+            'review'        =>$review_score,
+            'created_at'    =>date('Y-m-d H:i:s'),
+            ]);
+        
+        return $this->getMovie();
     }
 }
